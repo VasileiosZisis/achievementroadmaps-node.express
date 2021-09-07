94b9ffd37f7cb1ba7d046537e85882c97c618bff
GoneAway <vasil.zisis@gmail.com>
2021-09-07 21:25:51 +0300

    -

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const helmet = require("helmet");
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config ({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+});
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.static(path.join(__dirname, '/assets')));
+app.use(helmet({contentSecurityPolicy: false,}));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
