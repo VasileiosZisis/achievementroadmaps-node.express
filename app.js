71ad3c48d418d0fd3f03d3fc781b6c9f9d4d966b
GoneAway <vasil.zisis@gmail.com>
2022-02-15 21:52:46 +0200

    .

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
-if (process.env.NODE_ENV !=="production") {
-    require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const util = require('util');
-const path = require('path');
+const path = require("path");
 const helmet = require("helmet");
-const cloudinary = require('cloudinary').v2;
-const linksRouter = require('./routes/links');
+const cloudinary = require("cloudinary").v2;
+const linksRouter = require("./routes/links");
 
-cloudinary.config ({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
 
-app.use(express.static(path.join(__dirname, '/public')));
-app.use(express.static(path.join(__dirname, '/assets')));
-app.use(helmet({contentSecurityPolicy: false,}));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "/assets")));
+app.use(helmet({ contentSecurityPolicy: false }));
 app.use(linksRouter);
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 //sitemap
-app.get('/sitemap.xml', function( req, res, next ) {
-    res.header('Content-Type', 'application/xml');
-    res.render( 'sitemap' );
+app.get("/sitemap.xml", function (req, res, next) {
+  res.header("Content-Type", "application/xml");
+  res.render("sitemap");
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Serving ${port}`)
-})
+  console.log(`Serving ${port}`);
+});
--- steamAPI.js
+++ steamAPI.js
@@ -1,38 +1,32 @@
-const fs = require('fs')
-const SteamAPI = require('steamapi')
-const steam = new SteamAPI('3C128AC38062B1A86CD2A0E2E14F4D32')
+const fs = require("fs");
+const util = require("util");
+const SteamAPI = require("steamapi");
+const steam = new SteamAPI("3C128AC38062B1A86CD2A0E2E14F4D32");
 
-// steam.getGameSchema('814380').then(data => {
-//     console.log(util.inspect(data, { showHidden: true, depth: null, colors: true }));
-//     await fs.promises.writeFile('achievos.json', JSON.stringify(data));
-// });
+async function createFile(filename) {
+  this.filename = filename;
 
-class AchievementsList {
-  constructor (filename) {
-    if (!filename) {
-      throw new Error('no filename')
-    }
+  // try {
+  //   fs.accessSync(this.filename);
+  // } catch (err) {
+  //   fs.writeFileSync(this.filename, "[]");
+  // }
 
-    this.filename = filename
-
-    try {
-      fs.accessSync(this.filename)
-    } catch (err) {
-      fs.writeFileSync(this.filename, '[]')
-    }
-  }
-  async getFile () {
-    const contents = await fs.promises.readFile(this.filename, {
-      encoding: 'utf8'
-    })
-
-    console.log(contents)
-  }
+  steam.getGameSchema("814380").then((data) => {
+    // console.log(
+    //   util.inspect(data, { showHidden: true, depth: null, colors: true })
+    // );
+    fs.promises.writeFile(this.filename, JSON.stringify(data, null, 2));
+  });
 }
 
-const test = async () => {
-  const repo = new AchievementsList('achievos.json')
-  await repo.getFile()
-}
+createFile("sekiro.json");
 
-test()
+//   async getFile() {
+//     return JSON.parse(
+//       await fs.promises.readFile(this.filename, {
+//         encoding: "utf8",
+//       })
+//     );
+//   }
+// }
