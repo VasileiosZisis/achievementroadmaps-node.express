fd1314714455e8c1d3f365363369fdb952718db5
GoneAway <vasil.zisis@gmail.com>
2022-02-22 22:05:19 +0200

    .

--- steamAPI.js
+++ steamAPI.js
@@ -7,47 +7,33 @@ async function createFile(filename) {
   this.filename = filename;
 
   steam.getGameSchema("814380").then((data) => {
-    // parse json
-    const jsonObj = JSON.parse(data.availableGameStats.achievements);
-    console.log(jsonObj);
-    // data.availableGameStats.achievements.map(
-    //   (x) =>
-    //     fs.promises.writeFile(
-    //       this.filename,
-    //       JSON.stringify(x.displayName, null, 2)
-    //     )
-    //   console.log(
-    //     util.inspect(x.displayName, {
-    //       showHidden: true,
-    //       depth: null,
-    //       colors: true,
-    //     })
-    //   )
+    // console.log(
+    //   util.inspect(data, { showHidden: true, depth: null, colors: true })
     // );
-  });
 
-  // fs.promises.writeFile(this.filename, JSON.stringify(data, null, 2));
+    const achievements = JSON.parse(
+      JSON.stringify(data.availableGameStats.achievements, [
+        "displayName",
+        "description",
+        "icon",
+      ])
+    );
+
+    fs.writeFile(
+      "./public/ach_local/" + this.filename,
+      JSON.stringify(achievements, null, 2),
+      "utf8",
+      function (err) {
+        if (err) {
+          console.log("--- ERROR ---");
+          return console.log(err);
+        }
+        console.log(
+          "--- " + `${achievements.length}` + " achievements saved ---"
+        );
+      }
+    );
+  });
 }
 
-steam.getGameSchema("814380").then((data) => {
-  const firstObject = { ...data.availableGameStats.achievements };
-  for (const prop in firstObject) {
-    const { name, defaultvalue, hidden, icongray, ...secondObject } =
-      firstObject[prop];
-    console.log(secondObject);
-  }
-
-  // stringify JSON Object
-  // const jsonContent = JSON.stringify(picked, null, 2);
-  // console.log(jsonContent);
-  // fs.writeFile("output.json", jsonContent, "utf8", function (err) {
-  //   if (err) {
-  //     console.log("An error occured while writing JSON Object to File.");
-  //     return console.log(err);
-  //   }
-
-  //   console.log("JSON file has been saved.");
-  // });
-});
-
-// createFile("sekiro.json");
+// createFile("broforce.json");
